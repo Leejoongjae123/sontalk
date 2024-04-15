d99934a41b089703e46e77d0019c2e6f4e080caf
ljj33 <ljj3347@naver.com>
2024-04-15 17:07:51 +0900

    240415_11

--- app/(client)/reset/page.js
+++ app/(client)/reset/page.js
@@ -37,9 +37,9 @@ export default function Reset({
     })
 
     console.log("2:",error)
-    if (!error){
-      return redirect("/?loginsuccess=true")
-    }
+    // if (!error){
+    //   return redirect("/?loginsuccess=true")
+    // }
 
 
   };
